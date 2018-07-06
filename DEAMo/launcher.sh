9f3c295421c2e9199ab09cc98addf925ecb6cd56
danielpaulotipan <daniel_tipan@dlsu.edu.ph>
2018-07-06 12:23:24 +0800

    Update launcher.sh

--- DEAMo/launcher.sh
+++ DEAMo/launcher.sh
@@ -1,2 +1,14 @@
 #!/bin/sh
 sudo python /home/pi/gas.py
+
+# Open terminal
+# Type sudo nano /home/pi/launcher.sh
+# input the lines of launcher.sh(The code above)
+# ctrl x, y, enter.
+# in terminal, type cd /home/pi
+# type chmod 755 launcher.sh
+# type sudo nano /home/pi/.config/lxsession/LXDE-pi/autostart
+# input this into the bottom of the file
+#@sh /home/pi/launcher.sh
+# ctrl x, y, enter.
+# sudo reboot

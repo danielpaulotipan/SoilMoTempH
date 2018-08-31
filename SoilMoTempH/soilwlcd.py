3e982a03f6c9783b4e624a14d92f369452633afb
danielpaulotipan <daniel_tipan@dlsu.edu.ph>
2018-08-31 13:46:21 +0800

    Add files via upload

--- SoilMoTempH/soilwlcd.py
+++ SoilMoTempH/soilwlcd.py
@@ -32,7 +32,7 @@ adc = Adafruit_ADS1x15.ADS1115()
 GAIN = 2/3
 
 #File
-filename = "/home/pi/soil_data/soil " + "{:%H:%M %m-%d-%Y}".format(datetime.datetime.now()) + ".txt"
+filename = "/home/pi/soil_data/soil " + "{:%H_%M %m_%d_%Y}".format(datetime.datetime.now()) + ".txt"
 file = open(filename, "a")
 file.write("Time, Temp, Moisture, pH,")
 file.write('\n')
@@ -42,7 +42,7 @@ file.write('\n')
 #Define temperature Sensor
 def Shutdown(channel):
 	try:
-		os.system("sudo cp -a /home/pi/soil_data/. /media/pi/usb1")
+		os.system("sudo cp -a /home/pi/soil_data/. /media/pi/USB1")
 	except:
 		pass
 	display.lcd_display_string("Good Bye!!!         ", 1)
@@ -50,9 +50,9 @@ def Shutdown(channel):
 	display.lcd_display_string("{:%Y-%m-%d %H:%M:%S}  ".format(datetime.datetime.now()), 3)
 	display.lcd_display_string("Wait for 10 seconds ", 4)
 	display.backlight(0)
-	time.sleep(3)
+	time.sleep(5)
 
-	os.system("sudo killall python && sudo shutdown -h now")
+	os.system("sudo killall python && sudo poweroff")
 
 def read_temp_raw():
     f = open(device_file, 'r')
@@ -74,48 +74,45 @@ def read_temp():
 
 ######################################################u
 #Print
+GPIO.add_event_detect(4, GPIO.FALLING, callback = Shutdown, bouncetime = 2000)
 
 while True:
-	now = datetime.datetime.now()
-	time.sleep(60 - now.second - now.microsecond / 1e6)	
-	
-	while True:
 
-		GPIO.output(18,GPIO.HIGH)
+	GPIO.output(18,GPIO.HIGH)
 
-		values  = [0]*4
-		values2 = [1]*4
+	values  = [0]*4
+	values2 = [1]*4
 
-	        values[0] = adc.read_adc(0, gain=GAIN)
-	        vmoi = (values[0] * 0.1875)/ 1000
-		vmois = 100 - ((vmoi / 4.8) * 100)
-		vmoistperc = float(round((vmois), 2))
+       	values[0] = adc.read_adc(0, gain=GAIN)
+        vmoi = (values[0] * 0.1875)/ 1000
+	vmois = 100 - ((vmoi / 4.8) * 100)
+	vmoistperc = float(round((vmois), 2))
 
-		if vmoistperc >= 100:
-			vmoistperc = 100
-			vmoistperc = str(vmoistper)
-		elif vmoistperc <= 0:
-			vmoistperc = 0
-				vmoistperc = str(vmoistperc)
-		else:
-			vmoistperc = str(vmoistperc)
+	if vmoistperc >= 100:
+		vmoistperc = 100
+		vmoistperc = str(vmoistper)
+	elif vmoistperc <= 0:
+		vmoistperc = 0
+		vmoistperc = str(vmoistperc)
+	else:
+		vmoistperc = str(vmoistperc)
 
-		offset2 = 0.3
-		values2[1] = adc.read_adc(1, gain=GAIN)
-		vsoilp = ((values2[1] * 0.1875) / 1000)*3.5
-		vsoilph = str(round((vsoilp - offset2),2))
+	offset2 = 0.3
+	values2[1] = adc.read_adc(1, gain=GAIN)
+	vsoilp = ((values2[1] * 0.1875) / 1000)*3.5
+	vsoilph = str(round((vsoilp - offset2),2))
 
-		display.lcd_display_string("{:%H:%M %m-%d-%Y}".format(datetime.datetime.now()), 1)
-		display.lcd_display_string("Soil  pH : {0:>6}".format(vsoilph), 2)
-		display.lcd_display_string("Moisture : %s %%" % vmoistperc, 3)
-		display.lcd_display_string("Temp degC: %s" % read_temp(), 4)
+	display.lcd_display_string("{:%H:%M %m-%d-%Y}".format(datetime.datetime.now()), 1)
+	display.lcd_display_string("Soil  pH : {0:>6}".format(vsoilph), 2)
+	display.lcd_display_string("Moisture : %s %%" % vmoistperc, 3)
+	display.lcd_display_string("Temp degC: %s" % read_temp(), 4)
 
-		file.write("{:%Y-%m-%d %H:%M:%S},".format(datetime.datetime.now()))
-		file.write("%s," % read_temp() + "%s," % vmoistperc)
-		file.write("{0:>6}".format(vsoilph))
-		file.write("\n")
-		file.flush()
+	file.write("{:%Y-%m-%d %H:%M},".format(datetime.datetime.now()))
+	file.write("%s," % read_temp() + "%s," % vmoistperc)
+	file.write("{0:>6}".format(vsoilph))
+	file.write("\n")
+	file.flush()
 
-		GPIO.output(18,GPIO.LOW)
+	GPIO.output(18,GPIO.LOW)
 
-		time.sleep(5.0 - ((time.time() - starttime) % 5.0))
+	time.sleep(60)
